783d266ed54bd3f9e624de345e53ea940f21def5
dreayoung <youngmandrea@gmail.com>
2024-02-07 01:16:29 -0500

    first draft retry

--- components/Results.js
+++ components/Results.js
@@ -1,42 +1,8 @@
-import React from 'react';
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from '@/components/ui/drawer';
-import gift from '../public/gift.png';
 import Image from 'next/image';
+import gift from '../public/images/gift.png';
 
 function Results() {
-  return (
-    <Drawer>
-      <DrawerTrigger asChild>
-        <Button variant="outline">Open Drawer</Button>
-      </DrawerTrigger>
-      <DrawerContent>
-        <div className="mx-auto w-full max-w-sm">
-          <DrawerHeader>
-            <DrawerTitle>name loves crush</DrawerTitle>
-            <DrawerDescription>Are you meant to be?</DrawerDescription>
-          </DrawerHeader>
-          <div className="p-4 pb-0">
-            <Image src={gift} alt="gift" className="w-32" />
-          </div>
-          <DrawerFooter>
-            <Button>Submit</Button>
-            <DrawerClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DrawerClose>
-          </DrawerFooter>
-        </div>
-      </DrawerContent>
-    </Drawer>
-  );
+  return <div></div>;
 }
 
 export default Results;
