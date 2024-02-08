d51d0880d570e11d5ad4a53e101ca97a725b5462
dreayoung <youngmandrea@gmail.com>
2024-02-08 00:23:15 -0500

    balloon

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { Lilita_One, Monofett } from 'next/font/google';
 import handLove from '../public/images/hand_with_love.png';
 import heart from '../public/images/hearlock1.svg';
 import loveHeart from '../public/images/loveeee.svg';
-import hotairballoon from '../public/images/air_balloon_with_clouds.png';
+import hotairballoon from '../public/images/airballoon.png';
 
 import Footer from '@/components/Footer';
 import LoveForm from '@/components/LoveForm';
